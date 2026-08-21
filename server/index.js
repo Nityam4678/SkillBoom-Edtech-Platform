3cfaf8e0c141a16d75bfb309f62d68e87497fc9f
Nityam4678 <npp4678@gmail.com>
2026-08-21 15:59:50 +0530

    fix trust proxy

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.set("trust proxy", 1);
 
 const userRoutes = require("./routes/User");
 const profileRoutes = require("./routes/Profile");
